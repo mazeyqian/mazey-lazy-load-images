112d45d9babde82213f7b1912ad6cba3eb4947e1
钱程 <mazey@mazey.net>
2022-04-30 17:55:04 +0800

    feat: release 0.0.1

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,78 @@
 import {
-  foo
+  lazyLoadImages
  } from './index';
 
 // 数字测试
-console.log('Test:', foo('foo', 'bar'));
+// console.log('Test:', foo('foo', 'bar'));
+/*
+https://blog.mazey.net/wp-content/uploads/2022/03/Arkwrights-Cotton-Mills-by-night-1790s.jpg
+https://blog.mazey.net/wp-content/uploads/2022/04/68747470733a2f2f7261772e6769746875622e636f6d2f66656e676d6b322f6b6f612d67756964652f6d61737465722f6f6e696f6e2e706e67.png
+https://blog.mazey.net/wp-content/uploads/2022/03/2B6D5045-E4CF-4B00-BAD7-8FC93FB9DDFE.png
+https://blog.mazey.net/wp-content/uploads/2022/03/D13A884F-2898-486E-BFA3-D14E3AAF9988.png
+https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg
+https://blog.mazey.net/wp-content/uploads/2021/12/EmotionormalBanner-534x228-1.jpg
+https://blog.mazey.net/wp-content/uploads/2021/12/ins-20210409-pwa-w-400.jpg
+https://blog.mazey.net/wp-content/uploads/2021/12/1495120456257031.png
+
+*/
+
+lazyLoadImages({
+  images: [
+    {
+      name: 'Example1',
+      img: [
+        'https://i.mazey.net/asset/default/suzumiya-haruhi1.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2022/03/2B6D5045-E4CF-4B00-BAD7-8FC93FB9DDFE.png',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2022/03/Arkwrights-Cotton-Mills-by-night-1790s.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2022/04/68747470733a2f2f7261772e6769746875622e636f6d2f66656e676d6b322f6b6f612d67756964652f6d61737465722f6f6e696f6e2e706e67.png',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2022/03/2B6D5045-E4CF-4B00-BAD7-8FC93FB9DDFE.png',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2022/03/D13A884F-2898-486E-BFA3-D14E3AAF9988.png',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2021/12/EmotionormalBanner-534x228-1.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2021/12/ins-20210409-pwa-w-400.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2021/12/1495120456257031.png',
+        'https://blog.mazey.net/wp-content/uploads/2022/03/D13A884F-2898-486E-BFA3-D14E3AAF9988.png',
+        'https://blog.mazey.net/wp-content/uploads/2022/01/bg-psc-202201030212-2880x1136-1-scaled.jpg',
+      ]
+    },
+    {
+      name: 'Example1',
+      img: [
+        'https://blog.mazey.net/wp-content/uploads/2021/12/EmotionormalBanner-534x228-1.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2021/12/ins-20210409-pwa-w-400.jpg',
+        'https://blog.mazey.net/wp-content/uploads/2021/12/1495120456257031.png',
+      ]
+    },
+  ],
+  container: '.box',
+  defaultImg: 'https://i.mazey.net/asset/default/201909170739.jpg'
+});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,94 @@
 /* Function */
 
+import { throttle, addInlineStyle } from 'mazey';
+
 /**
- * @method foo
- * @description Balabala
- * @param {String} aStr 字符串
- * @param {String} bStr 字符串
- * @return {Number} 长度
+ * @method lazyLoadImages
+ * @description Lazey load images.
+ * @param {Array} images Data containing images.
+ * [
+    {
+      name: 'Example1',
+      img: [
+        'https://i.mazey.net/asset/default/suzumiya-haruhi1.jpg',
+      ]
+    }
+   ]
+ * @param {String} container A String can be used by `document.querySelector()`, such as: `.box` or `#entry-content`.
+ * @param {String} defaultImg Default load image, such as: `https://i.mazey.net/asset/default/201909170739.jpg`.
+ * @return {Boolean} true or false
  */
-export function foo(aStr: string, bStr: string): number {
-  const aLen = aStr.length;
-  const bLen = bStr.length;
-  return aLen + bLen;
+export function lazyLoadImages({images = [{ name: '', img: [''] }], container = '', defaultImg = ''} = {}): boolean {
+  const entryContent = document.querySelector(container);
+  const items = images.reduce((outerItems, { name = '', img = [] }, index) => {
+    // Parse image list
+    const imgItems = img.reduce((innerItems, imgSrc) => {
+      innerItems += `
+        <div><img src="${defaultImg}" data-src="${imgSrc}" class="m-img-item" loading="lazy"></div>
+      `;
+      return innerItems;
+    }, '');
+    // Parse item list
+    outerItems += `
+      <div>
+        <div>
+          <span>${index + 1}. ${name}</span>
+        </div>
+        <div class="m-img">
+          ${imgItems}
+        </div>
+      </div>
+    `;
+    return outerItems;
+  }, '');
+  // Content
+  const box = `
+    <div class="m-box">
+      ${items}
+    </div>
+  `;
+  if (entryContent) {
+    entryContent.innerHTML = box;
+    // Lazy load
+    const lazyImages = [...document.querySelectorAll('.m-img-item') as any];
+    const inAdvance = 300;
+    const lazyLoad = () => {
+      lazyImages.forEach(image => {
+        if (image.offsetTop < window.innerHeight + window.pageYOffset + inAdvance) {
+          image.src = image.dataset.src;
+        }
+      });
+    };
+    // Listen
+    window.addEventListener('scroll', throttle(lazyLoad, 50, {}));
+    window.addEventListener('resize', throttle(lazyLoad, 50, {}));
+    // Load default image first.
+    const imgInstance = new Image();
+    imgInstance.onload = () => {
+      Promise.resolve().then(() => {
+        lazyLoad();
+      });
+    };
+    imgInstance.src = defaultImg;
+  } else {
+    return false;
+  }
+  addInlineStyle({
+    inlineStyle: `
+      .m-box {
+        box-sizing: border-box;
+      }
+      .m-img {
+        column-count: 2;
+        font-size: 0;
+      }
+      .m-img-item {
+        width: 100%;
+        min-height: 50px;
+        border-radius: 4px;
+      }
+    `,
+    id: 'test',
+  });
+  return true;
 }
